34db1fc036f2c2344db2416c6c329ccb785d6335
Mayank Choudhary <56837137+mayankchaudhary26@users.noreply.github.com>
2021-10-29 21:35:20 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,8 +5,6 @@ import pandas as pd
 import seaborn as sns
 import os
 
-# Importing Deep Learning Libraries
-
 from keras.preprocessing.image import load_img, img_to_array
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Dense,Input,Dropout,GlobalAveragePooling2D,Flatten,Conv2D,BatchNormalization,Activation,MaxPooling2D
